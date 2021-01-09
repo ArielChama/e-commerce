c72bba5c5e38088439cef7e99b117db59f0889ee
ArielChama <arielchama45@gmail.com>
2021-01-09 20:37:46 +0100

    Alterando tabela 'products'

--- database/migrations/2021_01_03_011824_create_products_table.php
+++ database/migrations/2021_01_03_011824_create_products_table.php
@@ -18,6 +18,7 @@ class CreateProductsTable extends Migration
             $table->string('name');
             $table->text('description');
             $table->double('price');
+            $table->string('image');
             $table->timestamps();
         });
     }
