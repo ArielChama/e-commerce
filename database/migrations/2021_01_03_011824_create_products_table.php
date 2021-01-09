5aa8cf70b55ca54131769c2211a259d36ecf20a0
ArielChama <arielchama45@gmail.com>
2021-01-09 02:16:11 +0100

    Criando migração da tabela 'products'

--- database/migrations/2021_01_03_011824_create_products_table.php
+++ database/migrations/2021_01_03_011824_create_products_table.php
@@ -15,6 +15,9 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->double('price');
             $table->timestamps();
         });
     }
