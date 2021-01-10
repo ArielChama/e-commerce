4ff9b3bb91303dbaa3e790093d905a84864ce303
ArielChama <arielchama45@gmail.com>
2021-01-10 03:14:00 +0100

    Melhorando

--- routes/web.php
+++ routes/web.php
@@ -23,9 +23,3 @@ Route::group(['prefix' => 'products'], function () {
     Route::get('/', 'productsController@products')->name('products.list');
     Route::get('/{id}', 'productsController@view')->name('products.view');
 });
-
-Route::group(['prefix' => 'admin', 'namespace' => 'admin'], function () {
-    Route::get('/', 'adminController@login')->name('admin.login');
-    Route::get('/dashboard', 'adminController@dashboard')->name('admin.dashboard');
-    Route::post('/publish', 'admin\adminController@publish');
-});
